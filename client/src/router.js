db6ed55141690fddebbe1aa27073b638712e1fc7
Shueny <shuen29@gmail.com>
2019-07-28 21:59:32 +0800

    把index設定在 router 中

--- client/src/router.js
+++ client/src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Index from './views/index.vue';
 
 Vue.use(Router);
 
@@ -7,8 +8,8 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [{
-    // path: '/',
-    // name: 'home',
-    // component: Home,
+    path: '/',
+    name: 'index',
+    component: Index,
   }],
 });
