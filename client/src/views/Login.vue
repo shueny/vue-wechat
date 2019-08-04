d8b160ff9af4479edbb402261c6c9a68b8767344
Shueny <shuen29@gmail.com>
2019-08-04 23:56:05 +0800

    新增Login and Register 的components

---
+++ client/src/views/Login.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="login">Login Page</div>
+</template>
+<script>
+export default {
+  name: "Login",
+  components: {}
+};
+</script>
---
+++ client/src/views/Register.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="register">Register Page</div>
+</template>
+<script>
+export default {
+  name: "Register",
+  components: {}
+};
+</script>
