e541bd9e3552832d97b6b6d76329d632fcfee443
Shueny <shuen29@gmail.com>
2019-08-04 23:55:01 +0800

    修改 router 設定

--- client/src/router.js
+++ client/src/router.js
@@ -11,5 +11,16 @@ export default new Router({
     path: '/',
     name: 'index',
     component: Index,
-  }],
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('./views/Login.vue'),
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('./views/Register.vue'),
+  },
+  ],
 });
