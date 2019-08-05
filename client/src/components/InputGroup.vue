c8661089a97578060ba6f094f3e5dfa6afaa4194
Shueny <shuen29@gmail.com>
2019-08-05 17:27:47 +0800

    新增共用 input_group

---
+++ client/src/components/InputGroup.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="input_group">
+    <label for="name">{{ label }}</label>
+    <input
+      :type="type"
+      :value="value"
+      :placeholder="placeholder"
+      :name="name"
+      @input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: "input_group",
+  prop: {
+    type: {
+      type: String,
+      default: "text"
+    },
+    value: String,
+    placeholder: String,
+    name: String,
+    label: String
+  }
+};
+</script>
+<style lang="scss" scoped>
+.input_group {
+  width: 100%;
+  height: $font4 * 15;
+}
+</style>
