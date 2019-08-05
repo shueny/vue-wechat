d26930d5b4a835a6e9a5a98d737ca36e2d832816
Shueny <shuen29@gmail.com>
2019-08-05 17:31:52 +0800

    設定 input_group CSS

--- client/src/components/InputGroup.vue
+++ client/src/components/InputGroup.vue
@@ -29,5 +29,22 @@ export default {
 .input_group {
   width: 100%;
   height: $font4 * 15;
+  box-sizing: border-box;
+  border-bottom: 1px solid $light-grey;
+  padding: $font4 * 3 0;
+  line-height: $font4 * 15;
+
+  label {
+    display: inline-block;
+    width: 30%;
+    font-size: $font4 * 4;
+  }
+  input {
+    display: inline-block;
+    width: 65%;
+    font-size: $font4 * 4;
+    border: none;
+    outline: none;
+  }
 }
 </style>
